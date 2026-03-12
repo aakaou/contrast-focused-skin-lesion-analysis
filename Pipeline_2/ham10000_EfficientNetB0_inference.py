694ce05aea4036983cab223fbc826d7045648369
aboubakr aakaou <126001077+aakaou@users.noreply.github.com>
2026-03-12 23:58:01 +0100

    Update ham10000_EfficientNetB0_inference.py

--- Pipeline_2/ham10000_EfficientNetB0_inference.py
+++ Pipeline_2/ham10000_EfficientNetB0_inference.py
@@ -12,10 +12,10 @@ from tensorflow.keras.models import Model   # Model class used to construct neur
 # Paths
 # =====================================================
 
-PROC_DIR = Path("/home/aboubakr/Descargas/article4/ham10000/pipeline2/HAM10000_segmented_images")
+PROC_DIR = Path("/aakaou/ham10000/pipeline2/HAM10000_segmented_images")
 # Directory containing segmented HAM10000 skin lesion images
 
-OUTPUT_CSV = "/home/aboubakr/Descargas/article4/ham10000/pipeline2/ham10000_efficientnetb0_segmented_predictions_p2.csv"
+OUTPUT_CSV = "/aakaou/ham10000/pipeline2/ham10000_efficientnetb0_segmented_predictions_p2.csv"
 # CSV file where prediction results will be saved
 
 IMG_SIZE = (224, 224)                       # Input image size required by EfficientNetB0
