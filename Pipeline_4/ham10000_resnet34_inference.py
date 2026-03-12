538138fe2e1f71673f010cf292661bb9312bc8c2
aboubakr aakaou <126001077+aakaou@users.noreply.github.com>
2026-03-13 00:40:14 +0100

    Update ham10000_resnet34_inference.py

--- Pipeline_4/ham10000_resnet34_inference.py
+++ Pipeline_4/ham10000_resnet34_inference.py
@@ -23,9 +23,9 @@ import seaborn as sns              # Advanced plotting (confusion matrix & heatm
 # =========================
 # PATHS
 # =========================
-prediction_folder = Path("/aakaou/ham10000/pipeline3/seg_masks1")
-ground_truth_folder = Path("/aakaou/ham10000/pipeline3/seg_overlays1")
-csv_output = Path("/aakaou/ham10000/pipeline3/segmentation_metrics.csv")
+prediction_folder = Path("/aakaou/ham10000/pipeline4/seg_masks1")
+ground_truth_folder = Path("/aakaou/ham10000/pipeline4/seg_overlays1")
+csv_output = Path("/aakaou/ham10000/pipeline4/segmentation_metrics.csv")
 
 metadata_file = Path("/aakaou/ham10000/HAM10000_metadata.csv")
 original_images_folder = Path("/aakaou/ham10000/HAM10000_images_all")
