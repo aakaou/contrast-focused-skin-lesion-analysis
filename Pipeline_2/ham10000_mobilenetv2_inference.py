e022360a6a6c23aa6275e81fe7745158612128fc
aboubakr aakaou <126001077+aakaou@users.noreply.github.com>
2026-03-13 00:05:29 +0100

    Update ham10000_mobilenetv2_inference.py

--- Pipeline_2/ham10000_mobilenetv2_inference.py
+++ Pipeline_2/ham10000_mobilenetv2_inference.py
@@ -13,10 +13,10 @@ from tensorflow.keras.models import Model  # Keras Model class for building neur
 # Paths
 # =====================================================
 
-PROC_DIR = Path("/home/aboubakr/Descargas/article4/ham10000/pipeline2/HAM10000_segmented_images")  
+PROC_DIR = Path("/aakaou/ham10000/pipeline2/HAM10000_segmented_images")  
 # Directory containing segmented HAM10000 lesion images
 
-OUTPUT_CSV = "/home/aboubakr/Descargas/article4/ham10000/pipeline2/ham10000_mobilenetv2_segmented_predictions_p2.csv"  
+OUTPUT_CSV = "/aakaou/ham10000/pipeline2/ham10000_mobilenetv2_segmented_predictions_p2.csv"  
 # File where prediction results will be saved
 
 IMG_SIZE = (224, 224)                      # Input image size required by MobileNetV2
